7e561d7dae3691a79273c3f6a1f7f223b5bd93a6
David Becu <becu.david@pop.eu.com>
2020-10-07 17:15:30 +0200

    initial commit

--- js9/app.js
+++ js9/app.js
@@ -1,9 +1,39 @@
 //js9 pendu
-let writEvent = document.querySelector(".writeAera");
+let wordArrayCached = 'manger';
+let wordArray = [];
+let wordLength = wordArrayCached.length;
+let essai = 6;
 
-
-
-function writeAChar(c) {
-  document.addEventListener('keydown', function(event) {
-    
+for (let i = 0; i < wordLength; i++) {
+  wordArray.push('_');
 }
+
+console.log(wordArray);
+console.log("===============================================");
+document.body.addEventListener('keydown', function (event) {
+
+  if ( (wordArray.join('')) == wordArrayCached) {
+    console.log('Vous avez gagné!');
+  } else {
+    if (essai == 1) {
+      console.log('Vous avez perdu');
+    } else {
+      for (let i = 0; i < wordLength; i++) {
+        if (event.key == wordArrayCached[i]) {
+          wordArray[i] = event.key;
+        }
+      }
+      if (!(wordArrayCached.includes(event.key))) {
+        essai = --essai;
+        console.log('«'+event.key+'» n\'est pas une lettre contenue dans le mot secret.Nombre d\'essais restant : ' + essai);
+      }
+      console.log(wordArray);
+    }
+  }
+}
+
+);
+
+/* document.body.appendChild(document.createElement('span')).lastElementChild; */
+
+
